bf36b9f7e675090b0e08418f9cd2f1b999baee45
yousefmobelal <ym8370302@gmail.com>
2025-10-10 19:42:53 +0300

    use nav item component

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NavItem } from './children/nav-item/nav-item';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NavItem],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
