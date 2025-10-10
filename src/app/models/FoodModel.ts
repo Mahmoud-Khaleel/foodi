894490df8cee9a8f250b9d12445bc013b2af69ca
yousefmobelal <ym8370302@gmail.com>
2025-10-10 18:49:45 +0300

    show description of image

--- src/app/models/FoodModel.ts
+++ src/app/models/FoodModel.ts
@@ -5,6 +5,7 @@ export class FoodModel {
     public _id: string,
     public name: string,
     public image: string,
+    public description: string,
     public price: number,
     public restaurant: RestaurantModel,
   ) {}
