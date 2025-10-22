73baf40af5511af1952cfc79ab73fb8af3946dae
Yousef Mohamed <81933611+yousefmobelal@users.noreply.github.com>
2025-10-22 14:54:26 +0300

    add search for foods

--- src/app/components/foods/foods.ts
+++ src/app/components/foods/foods.ts
@@ -5,17 +5,18 @@ import { FoodsService } from '../../services/foods.service';
 import { Error } from '../error/error';
 import { FoodCardShimmer } from './children/food-card-shimmer/food-card-shimmer';
 import { FoodItem } from '../food-item/food-item';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-foods',
-  imports: [Error, FoodCardShimmer, FoodItem],
+  imports: [Error, FoodCardShimmer, FoodItem, FormsModule],
   templateUrl: './foods.html',
 })
 export class Foods implements OnInit {
   foods: FoodModel[] | null = null;
   error: string | null = null;
   skeletons = Array.from({ length: 10 });
-
+  searchTerm: string = '';
   foodsService = inject(FoodsService);
   router = inject(Router);
 
@@ -34,5 +35,12 @@ export class Foods implements OnInit {
         this.foods = null;
       },
     });
+  }
+
+  get filteredFoods(): FoodModel[] {
+    if (!this.foods) return [];
+    if (!this.searchTerm.trim()) return this.foods;
+    const term = this.searchTerm.toLowerCase();
+    return this.foods.filter((f) => f.name.toLowerCase().includes(term));
   }
 }
